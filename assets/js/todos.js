1e875c8da7343d760f4f12240b4ebb65f49f0037
IcemanXChill <tmega4420@gmail.com>
2018-08-02 23:54:00 -0400

    Added Inital commit.

---
+++ assets/js/todos.js
@@ -0,0 +1,20 @@
+//Check off Todo by Clicking.
+$("ul").on("click","li", function(){
+$(this).toggleClass("completed");
+});
+
+$("ul").on("click","span", function(event){
+	$(this).parent().fadeOut(400,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13){
+	var newtodo = $(this).val();
+	$(this).val("");
+	$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + newtodo + "</li>");
+
+	}
+});
